cc512822b0b3efc73bb4157531dd16a233b6a259
Soare Robert Daniel <soare.robert.daniel@protonmail.com>
2024-04-28 18:58:44 +0300

    small typo

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Find the scum master!';
+export const SITE_TITLE = 'Find the scrum master!';
 export const SITE_DESCRIPTION = 'Test your scrum master knowledge to find another scrum master!';
